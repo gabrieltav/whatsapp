8477eb8b086d6b3409e2ec144a122f6992ee07e2
Gabriel Tavares <tavaresgabriel963@gmail.com>
2024-03-14 22:29:22 -0400

    feat:  Creating a profile interface

--- app/src/main/java/com/gtavares/whatsapp/ProfileActivity.kt
+++ app/src/main/java/com/gtavares/whatsapp/ProfileActivity.kt
@@ -1,11 +1,27 @@
 package com.gtavares.whatsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gtavares.whatsapp.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityProfileBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
+        initialiseToolbar()
+    }
+
+    private fun initialiseToolbar() {
+        val toolbar = binding.includeToolbarProfile.tbMain
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = getString(R.string.edit_profile)
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
