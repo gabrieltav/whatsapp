5fa29694a5682e56d330d0a6f0ac35faf9af11b4
Gabriel Tavares <tavaresgabriel963@gmail.com>
2024-03-25 20:55:38 -0400

    feat: upload an image to Storage

--- app/src/main/java/com/gtavares/whatsapp/ProfileActivity.kt
+++ app/src/main/java/com/gtavares/whatsapp/ProfileActivity.kt
@@ -2,10 +2,13 @@ package com.gtavares.whatsapp
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import com.gtavares.whatsapp.databinding.ActivityProfileBinding
 import com.gtavares.whatsapp.utils.displayMessage
 
@@ -16,12 +19,19 @@ class ProfileActivity : AppCompatActivity() {
     }
     private var hasCameraPermission = false
     private var hasGalleryPermission = false
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val storage by lazy {
+        FirebaseStorage.getInstance()
+    }
 
     private val managerGallery = registerForActivityResult(
         ActivityResultContracts.GetContent()
     ) { uri ->
         if (uri != null) {
             binding.imageProfile.setImageURI(uri)
+            uploadImageStorage(uri)
         } else {
             displayMessage("Nenhuma imagem selacionada")
         }
@@ -34,6 +44,24 @@ class ProfileActivity : AppCompatActivity() {
         initialiseToolbar()
         requestPermissionsApp()
         initialiseEventClick()
+    }
+
+    private fun uploadImageStorage(uri: Uri) {
+        val userId = firebaseAuth.currentUser?.uid
+
+        if (userId != null) {
+            storage
+                .getReference("photos")
+                .child("users")
+                .child(userId)
+                .child("profile.jpg")
+                .putFile(uri)
+                .addOnSuccessListener { task ->
+                    displayMessage("Sucesso ao fazer upload da imagem")
+                }.addOnFailureListener {
+                    displayMessage("Erro ao fazer upload da imagem")
+                }
+        }
     }
 
     private fun initialiseEventClick() {
